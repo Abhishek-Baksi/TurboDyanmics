09951eb7110f124950aac5c9257fcb55ccbae50c
Abhishek-Baksi <abhisek.baksi@icloud.com>
2023-05-13 01:58:48 +0530

    index.js updated

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const jsonServer = require("json-server"); // importing json-server library
-const app = jsonServer.create();
+const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
-app.use(middlewares);
-app.use(router);
+server.use(middlewares);
+server.use(router);
 
-app.listen(port);
+server.listen(port);
